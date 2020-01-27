3c90bacc6b82449dc7d43b9be66a94335219c8be
Cooper Douglas Raterink <birdrink@gmail.com>
2020-01-26 20:44:59 -0800

    spinning stanford

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './stanford.png';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://en.wikipedia.org/wiki/Water_polo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          What is water polo?
         </a>
       </header>
     </div>
